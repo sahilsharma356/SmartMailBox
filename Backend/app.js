58277f41af5afda4c51e9acafcb7490bd9fdef61
shlyyzy <shirleyy0103@gmail.com>
2020-02-09 03:30:02 -0500

    added 0.0.0

--- Backend/app.js
+++ Backend/app.js
@@ -28,6 +28,6 @@ app.use("/data", dataRouter);
 
 
 // launch backend on API_PORT
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, '0.0.0.0', function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
