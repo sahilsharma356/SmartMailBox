274038b7bae8749fda1c9ef952ef53110855695a
shlyyzy <shirleyy0103@gmail.com>
2020-02-09 08:56:58 -0500

    Added name field

--- Backend/models/confirmed.customers.models.js
+++ Backend/models/confirmed.customers.models.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema; 
 
 const goodCustomers = new Schema ( 
-    {
+    {   name: {
+            type: String
+        },
         customer: {
             type: Number
         }
